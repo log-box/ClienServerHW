dd33acb34ebe4e3817074bcf16c307bfd90311b0
log-box <a.y.logvin@gmail.com>
2021-10-20 17:34:04 +0300

    lesson2 task3 done

--- lesson2.3.py
+++ lesson2.3.py
@@ -7,10 +7,12 @@ import yaml
 DATA_TO_YAML = {
     'list': [1, 'b'],
     'int': 123,
+    1:1,
     'dict': {
         '1а': 111,
         '1б': 222,
         '1в': 333,
+        '12':2
     }
 }
 
@@ -21,8 +23,8 @@ with open('test.yaml', 'r', encoding='utf8') as yaml_file:
     yaml_data = yaml.load(yaml_file, Loader=yaml.SafeLoader)
     key_errors = 0
     data_errors = 0
-    for key, value in yaml_data.items():
-        if key in DATA_TO_YAML:
+    for key, value in DATA_TO_YAML.items():
+        if key in yaml_data:
             print(f'[OK] key {key} is in original dict')
             if yaml_data[key] == DATA_TO_YAML[key]:
                 print(f'[OK] Data on key {key} is equal to original data')
@@ -33,6 +35,7 @@ with open('test.yaml', 'r', encoding='utf8') as yaml_file:
                 data_errors += 1
         else:
             print(f'[ERR] key {key} is not in original dict')
+            print('----------------------')
             key_errors += 1
     if (key_errors + data_errors) == 0:
         print('[OK] There no Errors during save and load data to YAML file')
